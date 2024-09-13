fab01a2176902df9821371c0ece0f5dbb47e702e
sakhrao <90141993+sakhrao@users.noreply.github.com>
2024-09-13 09:00:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let elementsArray = document.querySelectorAll(".reveal");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn();
+
+
+
+
+function closepreloader(){
+
+document.getElementById("preloader").style.display = 'none';
+
+}
+
+window.addEventListener("load",function(){ setTimeout(closepreloader, 1000);
+
+});
